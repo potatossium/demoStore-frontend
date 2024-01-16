9a3166b00e5b01a7435fa168eb84f609956db3c9
potatossium <zhangyingzhuchn@gmail.com>
2024-01-17 00:23:11 +1300

    third time commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import {React, useState} from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+//import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Menu, Container } from "semantic-ui-react";
 import Customers from "./Customers";
 import Products from "./Products";
